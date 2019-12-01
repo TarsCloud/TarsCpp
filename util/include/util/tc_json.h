1e3bb68aaff4b62130da428757766c3475ebd921
ruanshudong <ruanshudong@qq.com>
2019-12-01 09:32:04 +0800

    tc_json add License

--- util/include/util/tc_json.h
+++ util/include/util/tc_json.h
@@ -1,4 +1,20 @@
-﻿#ifndef __TC_JSON_H
+﻿/**
+ * Tencent is pleased to support the open source community by making Tars available.
+ *
+ * Copyright (C) 2016THL A29 Limited, a Tencent company. All rights reserved.
+ *
+ * Licensed under the BSD 3-Clause License (the "License"); you may not use this file except 
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * https://opensource.org/licenses/BSD-3-Clause
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed 
+ * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+ * specific language governing permissions and limitations under the License.
+ */
+
+#ifndef __TC_JSON_H
 #define __TC_JSON_H
 
 #include <string>
@@ -16,11 +32,6 @@ using namespace std;
 
 namespace tars
 {
-
-/////////////////////////////////////////////////
-// 说明: json编解码的公共库
-// Author : zhangcunli@tencent.com              
-/////////////////////////////////////////////////
 
 /**
 * 编解码抛出的异常
