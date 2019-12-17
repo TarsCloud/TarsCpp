54b522ad808f4bdd29790c0028de764187446c4b
ruanshudong <ruanshudong@qq.com>
2019-12-17 13:54:30 +0800

    add tc_platform.h for future to support mac&windows

---
+++ util/include/util/tc_platform.h
@@ -0,0 +1,44 @@
+﻿/**
+ * Tencent is pleased to support the open source community by making Tars available.
+ *
+ * Copyright (C) 2016THL A29 Limited, a Tencent company. All rights reserved.
+ *
+ * Licensed under the BSD 3-Clause License (the "License"); you may not use this file except 
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * https://opensource.org/licenses/BSD-3-Clause
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed 
+ * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+ * specific language governing permissions and limitations under the License.
+ */
+ 
+#ifndef _TC_PLATFORM_H_
+#define _TC_PLATFORM_H_
+
+#if defined _WIN32 || defined _WIN64
+#define TARGET_PLATFORM_WINDOWS		1
+
+#elif __APPLE__
+#define TARGET_PLATFORM_IOS			1
+
+#elif defined ANDROID
+#define TARGET_PLATFORM_ANDROID		1
+#define TARGET_PLATFORM_LINUX		1
+
+#elif __linux__
+#define TARGET_PLATFORM_LINUX		1
+
+#else
+#error Unsupported platform.
+
+#endif
+
+
+#if TARGET_PLATFORM_WINDOWS
+#include <winsock2.h>
+#include <windows.h>
+#endif
+
+#endif
