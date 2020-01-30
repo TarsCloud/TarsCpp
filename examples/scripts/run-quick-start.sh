97c46b84117f8169197d824823bc2a7c7b433d89
jarodruan <jerrylucky@vip.qq.com>
2020-01-30 15:49:54 +0800

    update run-quick-start.sh

--- examples/scripts/run-quick-start.sh
+++ examples/scripts/run-quick-start.sh
@@ -8,10 +8,12 @@ echo ${WORKDIR}
 
 killall -9 QuickStartDemo
 
+echo "${WORKDIR}/../bin/QuickStartDemo --config=${WORKDIR}/../../examples/QuickStartDemo/HelloServer/Server/config.conf"
 ${WORKDIR}/../bin/QuickStartDemo --config=${WORKDIR}/../../examples/QuickStartDemo/HelloServer/Server/config.conf &
 
 sleep 3
 
+echo "${WORKDIR}/../bin/QuickStartDemoClient"
 ${WORKDIR}/../bin/QuickStartDemoClient
 
 
