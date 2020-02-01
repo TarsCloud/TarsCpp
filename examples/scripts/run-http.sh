0df7e704857ba4461a2f46aab727e1e3600db114
ruanshudong <ruanshudong@qq.com>
2020-02-01 14:28:38 +0800

    add run -http.sh, add UtilDemo

---
+++ examples/scripts/run-http.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "run-http.sh"
+
+WORKDIR=$(pwd)
+
+echo ${WORKDIR}
+
+killall -9 HttpServer 
+
+echo "start server: ${WORKDIR}/../bin/HttpServer --config=${WORKDIR}/../../examples/HttpDemo/HttpServer/config.conf &"
+
+${WORKDIR}/../bin/HttpServer --config=${WORKDIR}/../../examples/HttpDemo/HttpServer/config.conf &
+
+sleep 3
+
+echo "client: ${WORKDIR}/../bin/HttpClient"
+
+${WORKDIR}/../bin/HttpClient 2 10000
+
+
