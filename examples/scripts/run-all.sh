70e41b248655ce55204ed710ab6233e8b59ea561
jarodruan <jerrylucky@vip.qq.com>
2020-02-07 13:56:40 +0800

    update bat sh

---
+++ examples/scripts/run-all.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "run-all.sh"
+
+sh ../examples/scripts/run-quick-start.sh
+sh ../examples/scripts/run-http.sh
+sh ../examples/scripts/run-co.sh
+sh ../examples/scripts/run-push.sh
+
