7b8c53e2eeb1b99940dc38cd694b7ae491ab5264
ruanshudong <ruanshudong@qq.com>
2020-02-09 23:06:32 +0800

    add Http2Imp

---
+++ examples/HttpDemo/HttpServer/Http2Imp.cpp
@@ -0,0 +1,79 @@
+﻿/**
+ * Tencent is pleased to support the open source community by making Tars available.
+ *
+ * Copyright (C) 2016THL A29 Limited, a Tencent company. All rights reserved.
+ *
+ * Licensed under the BSD 3-Clause License (the "License"); you may not use this file except 
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * https://opensource.org/licenses/BSD-3-Clause
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed 
+ * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+ * specific language governing permissions and limitations under the License.
+ */
+
+#include "Http2Imp.h"
+#include "util/tc_http2session.h"
+#include "servant/Application.h"
+
+using namespace std;
+
+TC_SpinLock Http2Imp::_mutex;
+
+unordered_map<int32_t, TC_Http2Session*> Http2Imp::_http2Sessions;
+
+//////////////////////////////////////////////////////
+void Http2Imp::initialize()
+{
+    //initialize servant here:
+    //...
+}
+
+//////////////////////////////////////////////////////
+void Http2Imp::destroy()
+{
+    //destroy servant here:
+    //...
+}
+
+// typedef int (*ResponseFunc)(const Req_Type reqtype, 
+//                          const string &requri, 
+//                          const TC_Http::http_header_type &reqHeader, 
+//                          const string &reqBody,
+//                          int &resopnseStatus,
+//                          string &resopnseAbout,
+//                          TC_Http::http_header_type &responseHeader,
+//                          string &responseBody);
+
+int Http2Imp::doRequest(TarsCurrentPtr current, vector<char> &buffer)
+{
+    vector<int32_t> vtReqid;
+
+    TC_Http2Session::doRequest(current->getRequestBuffer(), vtReqid);
+    
+    cout << "doRequest:" << TC_Common::tostr(vtReqid.begin(), vtReqid.end(), ", ") << endl;
+
+    TC_Http2Session* session = getHttp2Session(current->getUId());
+
+    TC_Http2Session::Http2Response rsp;
+    rsp.status = 200;
+    rsp.about  = "OK";
+    rsp.body   = "response helloworld";
+
+    for(size_t i = 0; i < vtReqid.size(); i++)
+    {
+        session->doResponse(vtReqid[i], rsp, buffer);
+    }
+
+    cout << "response buffer size:" << buffer.size() << endl;
+    return 0;
+}
+
+int Http2Imp::doClose(TarsCurrentPtr current)
+{
+    delHttp2Session(current->getUId());
+
+    return 0;
+}
---
+++ examples/HttpDemo/HttpServer/Http2Imp.h
@@ -0,0 +1,97 @@
+﻿/**
+ * Tencent is pleased to support the open source community by making Tars available.
+ *
+ * Copyright (C) 2016THL A29 Limited, a Tencent company. All rights reserved.
+ *
+ * Licensed under the BSD 3-Clause License (the "License"); you may not use this file except 
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * https://opensource.org/licenses/BSD-3-Clause
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed 
+ * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+ * specific language governing permissions and limitations under the License.
+ */
+
+#ifndef _Http2Imp_H_
+#define _HttpImp_H_
+
+#include "servant/Application.h"
+#include "util/tc_spin_lock.h"
+#include "util/tc_http2session.h"
+
+/**
+ *
+ *
+ */
+class Http2Imp : public Servant
+{
+public:
+    /**
+     *
+     */
+    virtual ~Http2Imp() {}
+
+    /**
+     *
+     */
+    virtual void initialize();
+
+    /**
+     *
+     */
+    virtual void destroy();
+
+    /**
+     *
+     */
+    int doRequest(TarsCurrentPtr current, vector<char> &buffer);
+
+    /**
+     * close connection
+     */    
+    int doClose(TarsCurrentPtr current);
+
+    static TC_Http2Session *getHttp2Session(uint32_t uid)
+    {
+        TC_LockT<TC_SpinLock> lock(_mutex);
+
+        auto it = _http2Sessions.find(uid);
+
+        if(it != _http2Sessions.end())
+        {
+            return it->second;
+        }
+        return NULL;
+    }
+
+    static void addHttp2Session(uint32_t uid, TC_Http2Session* ptr)
+    {
+        TC_LockT<TC_SpinLock> lock(_mutex);
+
+        _http2Sessions[uid] = ptr;
+    }
+
+    static void delHttp2Session(uint32_t uid)
+    {
+        TC_LockT<TC_SpinLock> lock(_mutex);
+
+        auto it = _http2Sessions.find(uid);
+
+        if(it != _http2Sessions.end())
+        {
+            delete it->second;
+
+            _http2Sessions.erase(it);
+        }
+    }
+    
+protected:
+
+    static TC_SpinLock _mutex;
+
+    static unordered_map<int32_t, TC_Http2Session*> _http2Sessions;
+};
+/////////////////////////////////////////////////////
+#endif
