794a44e8ffa279e30a8b2caf4e6b47eeaf06a4be
ruanshudong <ruanshudong@qq.com>
2020-04-01 20:47:52 +0800

    try remove libncurses5

--- cmake/Thirdparty.cmake
+++ cmake/Thirdparty.cmake
@@ -19,19 +19,19 @@ ENDIF(UNIX)
 if (TARS_MYSQL)
     add_definitions(-DTARS_MYSQL=1)
 
-    IF(UNIX AND NOT APPLE)
-        FIND_PACKAGE(Curses) 
-        IF(NOT CURSES_FOUND)
-            SET(ERRORMSG "Curses library not found. Please install appropriate package,
-            remove CMakeCache.txt and rerun cmake.")
-            IF(CMAKE_SYSTEM_NAME MATCHES "Linux")
-                SET(ERRORMSG ${ERRORMSG} 
-                "On Debian/Ubuntu, package name is libncurses5-dev(apt-get install libncurses5-dev), on Redhat/Centos and derivates " 
-                "it is ncurses-devel (yum install ncurses-devel).")
-        ENDIF()
-        MESSAGE(FATAL_ERROR ${ERRORMSG})
-        ENDIF()
-   ENDIF()
+#    IF(UNIX AND NOT APPLE)
+#        FIND_PACKAGE(Curses) 
+#        IF(NOT CURSES_FOUND)
+#            SET(ERRORMSG "Curses library not found. Please install appropriate package,
+#            remove CMakeCache.txt and rerun cmake.")
+#            IF(CMAKE_SYSTEM_NAME MATCHES "Linux")
+#                SET(ERRORMSG ${ERRORMSG} 
+#                "On Debian/Ubuntu, package name is libncurses5-dev(apt-get install libncurses5-dev), on Redhat/Centos and derivates " 
+#                "it is ncurses-devel (yum install ncurses-devel).")
+#        ENDIF()
+#        MESSAGE(FATAL_ERROR ${ERRORMSG})
+#        ENDIF()
+#   ENDIF()
 
 endif ()
 
