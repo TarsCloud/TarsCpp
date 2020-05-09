c7f2b6492ed6199f973ee80b3c1b93e5494b677d
pingfang <pingfang@tencent.com>
2020-05-09 14:33:01 +0800

    fix bug : last line of http headers is ingored

--- util/src/tc_http.cpp
+++ util/src/tc_http.cpp
@@ -2317,13 +2317,15 @@ size_t TC_Http::parseHeaderString(const char *beginIt, const char *headerIt, TC_
 
 	bool first      = true;
 	auto lineStartIt= beginIt;
+    bool isLastLine = false;
 
 	while (true)
 	{
 		auto it = strnstr(lineStartIt, "\r\n", headerIt - lineStartIt);
 		if(it == NULL)
 		{
-			break;
+            it = headerIt;
+            isLastLine = true;
 		}
 
 		//first line ignore
@@ -2363,6 +2365,11 @@ size_t TC_Http::parseHeaderString(const char *beginIt, const char *headerIt, TC_
 		{
 			first = false;
 		}
+
+        if (isLastLine)
+        {
+            break;
+        }
 
 		lineStartIt = it + 2;//sep.size();
 	}
