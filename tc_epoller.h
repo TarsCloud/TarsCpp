b29cd6fcf8e7c19cb90ac0538269fe5683458806
Douwanna <33800612+Douwanna@users.noreply.github.com>
2020-06-02 18:54:53 +0800

    Update notes

---
+++ tc_epoller.h
@@ -0,0 +1,298 @@
+﻿/**
+ * Tencent is pleased to support the open source community by making Tars available.
+ *
+ * Copyright (C) 2016THL A29 Limited, a Tencent company. All rights reserved.
+ *
+ * Licensed under the BSD 3-Clause License (the "License"); you may not use this file except 
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * https://opensource.org/licenses/BSD-3-Clause
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed 
+ * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+ * specific language governing permissions and limitations under the License.
+ */
+
+#ifndef __TC_EPOLLER_H_
+#define __TC_EPOLLER_H_
+
+#include "util/tc_platform.h"
+#include "util/tc_socket.h"
+#include <cassert>
+
+#if TARGET_PLATFORM_IOS
+#include "sys/event.h"
+
+const int EPOLLIN = 0x0001;
+const int EPOLLOUT = 0x0002;
+const int EPOLLERR = 0x0004;
+
+typedef kevent64_s epoll_event;
+
+#else
+#include "sys/epoll.h"
+#endif
+
+namespace tars
+{
+/////////////////////////////////////////////////
+/** 
+ * @file  tc_epoller.h 
+ * @brief  epoll操作封装类 
+ * @brief  Epoll operation encapsulation class
+ */
+/////////////////////////////////////////////////
+
+/**
+* @brief epoll异常类
+* @brief epoll exception class
+*/
+struct TC_Epoller_Exception : public TC_Exception
+{
+   TC_Epoller_Exception(const string &buffer, int err) : TC_Exception(buffer, err) {};
+   ~TC_Epoller_Exception() {};
+};
+
+/**
+ * @brief epoller操作类，已经默认采用了EPOLLET方式做触发 
+ * @brief epoller operation class, EPOLLET has been used by default for triggering 
+ */
+class TC_Epoller
+{
+public:
+	/**
+	 * @brief 通知epoll从wait中醒过来 
+	 * @brief Notice epoll to wake up from 'wait'
+	 */
+    class NotifyInfo
+    {
+	public:
+        NotifyInfo();
+		~NotifyInfo();
+
+		/**
+		 * 初始化
+		 * Initialization
+		 */	
+        void init(TC_Epoller *ep);
+
+		/**
+		 * 添加关联数据
+		 * Add corresponding data
+		 */ 
+		void add(uint64_t data);
+
+		/**
+		 * 通知notify醒过来
+		 * Notice notify to wake up
+		 */ 
+        void notify();
+
+		/**
+		 * 释放掉
+		 * Diposit
+		 */ 
+        void release();
+
+		/**
+		 * 获取通知fd
+		 * Get notifyFd
+		 */ 
+		int notifyFd();
+
+		/**
+		 * 清除通知, 否则会一直响应
+		 * Clear up notice, otherwise, it will always respond
+		 */ 
+		// void clear();
+	protected:
+        //通知fd
+        TC_Socket _notify;
+		TC_Socket _notifyClient;
+		// bool      _clear = true;
+		TC_Epoller *_ep;
+
+		/*Events associated with the notification handle*/
+		uint64_t _data;		//关联到通知句柄的事件
+
+    };
+
+	/**
+	 * @brief 构造函数. 
+	 * @brief Constructor Function
+	 *  
+     * @param bEt 默认是ET模式，当状态发生变化的时候才获得通知
+	 * @param bEt The default is et mode, which is notified when the status changes
+	 */
+	TC_Epoller();
+
+	/**
+     * @brief 析够函数.
+	 * @brief Destructor
+	 */
+	~TC_Epoller();
+
+	/**
+	 * @brief 生成epoll句柄. 
+	 * @brief Generate epoll handle.
+	 *  
+     * @param max_connections epoll服务需要支持的最大连接数
+	 * @param max_connections Maximum number of connections the epoll service needs to support
+	 */
+	void create(int max_connections);
+
+	/**
+	 * @brief 释放资源
+	 *  
+     * @param 
+	 */
+	void close();
+
+	/**
+	 * @brief 添加监听句柄. 
+	 * @brief Add listening handle.
+	 *  
+     * @param fd    句柄
+	 * @param fd    handle
+     * @param data  辅助的数据, 可以后续在epoll_event中获取到
+	 * @param data  auxiliary data that can be obtained in epoll_event subsequently
+     * @param event 需要监听的事件EPOLLIN|EPOLLOUT
+	 * @param event Events to be listened on EPOLLIN|EPOLLOUT
+     *              
+	 */
+	void add(SOCKET_TYPE fd, uint64_t data, int32_t event);
+
+	/**
+	 * @brief 修改句柄事件. 
+	 * @brief Modify handle event
+	 *  
+     * @param fd    句柄
+	 * @param fd    handle
+     * @param data  辅助的数据, 可以后续在epoll_event中获取到
+	 * @param data  auxiliary data that can be obtained in epoll_event subsequently
+     * @param event 需要监听的事件EPOLLIN|EPOLLOUT
+	 * @param event Events to be listened on EPOLLIN|EPOLLOUT
+	 */
+	void mod(SOCKET_TYPE fd, uint64_t data, int32_t event);
+
+	/**
+	 * @brief 删除句柄事件. 
+	 * @brief Delete handle event.
+	 *  
+     * @param fd    句柄
+	 * @param fd    handle
+     * @param data  辅助的数据, 可以后续在epoll_event中获取到
+	 * @param data  auxiliary data that can be obtained in epoll_event subsequently
+     * @param event 需要监听的事件EPOLLIN|EPOLLOUT
+	 * @param event Events to be listened on EPOLLIN|EPOLLOUT
+	 */
+	void del(SOCKET_TYPE fd, uint64_t data, int32_t event);
+
+	/**
+	 * @brief 等待时间. 
+	 * @brief wait time
+	 *  
+	 * @param millsecond 毫秒 
+     * @return int       有事件触发的句柄数
+	 * @return int       Number of handles triggered by events
+	 */
+	int wait(int millsecond);
+
+	/**
+     * @brief 获取被触发的事件.
+	 * @brief Get the triggered handle
+	 *
+	 * @return struct epoll_event&被触发的事件
+	 * @return Struct epoll_event& triggered event
+	 */
+	epoll_event& get(int i);// { assert(_pevs != 0); return _pevs[i]; }
+
+	/**
+     * @brief 是否有读事件
+	 * @brief whether it have event to read
+	 *
+	 * @return
+	 */		
+	static bool readEvent(const epoll_event &ev);
+
+	/**
+     * @brief 是否有写事件
+	 * @brief whether it have event to write
+	 *
+	 * @return
+	 */		
+	static bool writeEvent(const epoll_event &ev);
+
+	/**
+     * @brief 是否有异常事件
+	 * @brief whether it have eception event
+	 *
+	 * @return
+	 */		
+	static bool errorEvent(const epoll_event &ev);
+
+	/**
+     * @brief 获取低位/高位数据
+	 * @brief Get low/high bit data
+	 * @param high: true:高位, false:低位
+	 * @param high: true:high level, false:low level
+	 * @return
+	 */	
+	static uint32_t getU32(const epoll_event &ev, bool high);
+
+	/**
+     * @brief 获取64bit数据
+	 * @brief Get 64 bit data
+	 * @return
+	 */	
+	static uint64_t getU64(const epoll_event &ev);
+
+protected:
+
+	/**
+	 * @brief 控制epoll，将EPOLL设为边缘触发EPOLLET模式 
+	 * @brief Control epoll, set EPOLL to Edge Trigger EPOLLET mode
+     * @param fd    句柄，在create函数时被赋值
+	 * @param fd    Handle, assigned when creating function
+     * @param data  辅助的数据, 可以后续在epoll_event中获取到
+	 * @param data  auxiliary data that can be obtained in epoll_event subsequently
+     * @param event 需要监听的事件
+	 * @param event the event to be listened
+	 * @param op    EPOLL_CTL_ADD： 注册新的fd到epfd中； 
+	 * 			    EPOLL_CTL_MOD：修改已经注册的fd的监听事件； 
+	 * 			    EPOLL_CTL_DEL：从epfd中删除一个fd； 
+	 * @param op    EPOLL_CTL_ADD：Register new FD into EPFD
+	 * 				EPOLL_CTL_MOD：Modify the monitoring events for registered fd
+	 * 				EPOLL_CTL_DEL：Delete an FD from epfd
+	 *  
+	 */
+ 	void ctrl(SOCKET_TYPE fd, uint64_t data, uint32_t events, int op);
+
+protected:
+
+    /**
+     * 	epoll
+     */
+#if TARGET_PLATFORM_WINDOWS
+	void* _iEpollfd;
+#else
+    int _iEpollfd;
+#endif
+
+	/**
+     * 最大连接数
+	 * The max amount of connections
+	 */
+	int	_max_connections;
+
+	/**
+     * 事件集
+	 * Event Set
+	 */
+	epoll_event *_pevs;
+};
+
+}
+#endif
+
---
+++ tc_fifo.h
@@ -0,0 +1,161 @@
+﻿/**
+ * Tencent is pleased to support the open source community by making Tars available.
+ *
+ * Copyright (C) 2016THL A29 Limited, a Tencent company. All rights reserved.
+ *
+ * Licensed under the BSD 3-Clause License (the "License"); you may not use this file except 
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * https://opensource.org/licenses/BSD-3-Clause
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed 
+ * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+ * specific language governing permissions and limitations under the License.
+ */
+
+#ifndef __TC_FIFO_H
+#define __TC_FIFO_H
+
+#include "util/tc_platform.h"
+
+#if TARGET_PLATFORM_LINUX || TARGET_PLATFORM_IOS
+#include <string>
+
+namespace tars
+{
+
+/////////////////////////////////////////////////
+/** 
+ *@file   tc_fifo.h
+ *@brief  FIFO封装类. 
+ *@brief  FIFO encapsulation class
+ */
+/////////////////////////////////////////////////
+
+/** 
+ *@brief  管道操作类. 
+ *@brief  Pipeline operation.
+ */
+class TC_Fifo
+{
+public:
+
+   /**
+    * @brief 管道操作的枚举类型.  
+    * 定义了对管道的操作类型，EM_WRITE：写管道，EM_READ ：读管道
+    * @brief Enumeration types for pipeline operations
+    * Defines the type of operation on the pipeline, EM_ Write: write pipe, EM_ Read: read pipe
+    */
+    enum ENUM_RW_SET
+    {
+        EM_WRITE = 1,
+        EM_READ  = 2
+    };
+
+public:
+   /**
+    * @brief 构造函数. 
+    * @brief Constructor
+    *  
+    * @param bOwener : 是否拥有管道，默认为ture
+    * @param bOwener : Whether the pipeline is owned, the default is ture
+    */
+    TC_Fifo(bool bOwener = true);
+
+   /**
+    * @brief 析构函数. 
+    * @brief Constructor
+    */
+    ~TC_Fifo();
+
+public:
+    /**
+     * @brief 打开FIFO. 
+     * @brief Open FIFO
+     *  
+     * @param sPath 要打开的FIFO文件的路径 
+     * @param sPath the file path of the FIFO to open
+     * @param enRW  管道操作类型
+     * @param enRN  Pipe opration type
+     * @param mode  该FIFO文件的权限 ，默认为可读可写
+     * @param mode  The permissions of this FIFO file are read-write by default
+     * @return      0-成功,-1-失败 
+     * @return      0 - successfull, -1 - failed
+     */
+    int open(const std::string & sPath, ENUM_RW_SET enRW, mode_t mode = 0777);
+
+    /**
+     * @brief 关闭fifo 
+     * @brief Close FIFO
+     */
+    void close();
+
+    /**
+     * @brief 获取FIFO的文件描述符. 
+     * @brief Get the file descriptor of FIFO
+     * 
+     * @return  FIFO的文件描述符
+     * @return  the file descriptor of FIFO
+     */
+    int fd() const { return _fd; }
+
+    /**
+     * @brief 读数据, 当读取成功时，返回实际读取的字节数，如果返回的值是0，代表已经读到文件的结束；小于0表示出现了错误
+     * @brief read data, when read successfully, return the actual number of bytes read, if the value returned is 0, the end of the file has been read; less than 0 indicates an error 
+     * @param buffer     读取的内容
+     * @param buffer     the content to be read
+     * @param max_size   读取数据的大小 
+     * @param max_size   size of the data to be read
+     * @return           读到的数据长度 ,小于0则表示失败
+     * @return           the length of the read data , if it is less then 0, read failed
+     */
+    int read(char * szBuff, const size_t sizeMax);
+
+    /**
+     * @brief 向管道写数据. 
+     * @brief Write data to the pipeline
+     * 
+     * @param szBuff       要写入的数据
+     * @param szBuff       data to be wrote
+     * @param sizeBuffLen  数据的大小
+     * @param sizeBuffLen  size of data
+     * @return             大于0：表示写了部分或者全部数据 
+     *                        小于0：表示出现错误
+     * @return             geater than 0: Some or all of the data are shown and written 
+     *                     less than 0: Error
+     */
+    int write(const char * szBuff, const size_t sizeBuffLen);
+
+private:
+   /**
+    * FIFO文件的路径
+    * file path of FIFO
+    */
+    std::string        _sPathName;
+
+   /**
+    * 是否拥有FIFO
+    * whether it have FIFO
+    */
+    bool            _bOwner;
+
+   /**
+    * FIFO的文件的操作类型
+    * file opration type of FIFO
+    */
+    ENUM_RW_SET        _enRW;
+
+   /**
+    * FIFO的文件描述符
+    * file descriptor of FIFO
+    */
+    int             _fd;
+};
+
+}
+
+#endif
+
+#endif
+
