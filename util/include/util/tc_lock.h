9b34e2e950687cd3a47ff88128888de5486cf042
Douwanna <33800612+Douwanna@users.noreply.github.com>
2020-06-24 23:33:08 +0800

    Upload notes

--- util/include/util/tc_lock.h
+++ util/include/util/tc_lock.h
@@ -30,12 +30,14 @@ namespace tars
 /**
  * @file tc_lock.h 
  * @brief  锁类 
+ * @brief  Lock Class
  */           
 /////////////////////////////////////////////////
 
 
 /**
 * @brief  锁异常
+* @brief  Lock exception
 */
 struct TC_Lock_Exception : public TC_Exception
 {
@@ -46,6 +48,8 @@ struct TC_Lock_Exception : public TC_Exception
 /**
  * @brief  锁模板类其他具体锁配合使用，
  * 构造时候加锁，析够的时候解锁
+ * @brief  Lock template class with other specific locks, 
+ * lock when construction, unlock when analysis is sufficient
  */
 template <typename T>
 class TC_LockT
@@ -54,8 +58,10 @@ public:
 
     /**
      * @brief  构造函数，构造时枷锁
+     * @brief  Constructor, flail when constructing
      *  
      * @param mutex 锁对象
+     * @param mutex Lock Object
      */
     TC_LockT(const T& mutex) : _mutex(mutex)
     {
@@ -65,6 +71,7 @@ public:
 
     /**
      * @brief  析构，析构时解锁
+     * @brief  Destructor, unlock at destruct
      */
     virtual ~TC_LockT()
     {
@@ -76,6 +83,7 @@ public:
 
     /**
      * @brief  上锁, 如果已经上锁,则抛出异常
+     * @brief  Lock, throw an exception if already locked
      */
     void acquire() const
     {
@@ -89,8 +97,10 @@ public:
 
     /**
      * @brief  尝试上锁.
+     * @brief  Try to lock.
      *
      * @return  成功返回true，否则返回false
+     * @return  If it is successful,return true,otherwise return false
      */
     bool tryAcquire() const
     {
@@ -100,6 +110,7 @@ public:
 
     /**
      * @brief  释放锁, 如果没有上过锁, 则抛出异常
+     * @brief  Release lock, throw exception if no lock is on
      */
     void release() const
     {
@@ -113,8 +124,10 @@ public:
 
     /**
      * @brief  是否已经上锁.
+     * @brief  Is locked or not
      *
      * @return  返回true已经上锁，否则返回false
+     * @return  Returning true indicates that it is locked; otherwise returning false
      */
     bool acquired() const
     {
@@ -126,6 +139,8 @@ protected:
     /**
 	 * @brief 构造函数
 	 * 用于锁尝试操作，与TC_LockT相似
+     * @brief Constructor
+     * For lock attempt operations, with TC_LockT Similarity
 	 *  
      */
     TC_LockT(const T& mutex, bool) : _mutex(mutex)
@@ -142,17 +157,20 @@ private:
 protected:
     /**
      * 锁对象
+     * Lock object
      */
     const T&        _mutex;
 
     /**
      * 是否已经上锁
+     * Is it locked or not
      */
     mutable bool _acquired;
 };
 
 /**
  * @brief  尝试上锁
+ * @brief  Attempt to lock
  */
 template <typename T>
 class TC_TryLockT : public TC_LockT<T>
@@ -166,26 +184,32 @@ public:
 
 /**
  * @brief  空锁, 不做任何锁动作
+ * @brief  Empty lock, no lock action
  */
 class TC_EmptyMutex
 {
 public:
     /**
 	* @brief  写锁.
+    * @brief  Write lock
 	*  
     * @return int, 0 正确
+    * @return int  0  right
     */
     int lock()  const   {return 0;}
 
     /**
     * @brief  解写锁
+    * @brief  Unlock
     */
     int unlock() const  {return 0;}
 
     /**
 	* @brief  尝试解锁. 
+    * @brief  Try to unlock
 	*  
     * @return int, 0 正确
+    * @return int, 0 right
     */
     bool trylock() const {return true;}
 };
@@ -193,6 +217,8 @@ public:
 /**
  * @brief  读写锁读锁模板类
  * 构造时候加锁，析够的时候解锁
+ * @brief  Read-Write Lock Read Lock Template Class
+ * Lock when construction, unlock when analysis is sufficient
  */
 
 template <typename T>
@@ -201,8 +227,10 @@ class TC_RW_RLockT
 public:
     /**
 	 * @brief  构造函数，构造时枷锁
+     * @brief  Constructor, flail when constructing
 	 *
      * @param lock 锁对象
+     * @param lock lock object
      */
 	TC_RW_RLockT(T& lock)
 		: _rwLock(lock),_acquired(false)
@@ -213,6 +241,7 @@ public:
 
     /**
 	 * @brief 析构时解锁
+     * @brief Unlock at destruct time
      */
 	~TC_RW_RLockT()
 	{
@@ -224,11 +253,13 @@ public:
 private:
 	/**
 	 *锁对象
+     *lock object
 	 */
 	const T& _rwLock;
 
     /**
      * 是否已经上锁
+     * Is it locked or not
      */
     mutable bool _acquired;
 
@@ -242,8 +273,10 @@ class TC_RW_WLockT
 public:
     /**
 	 * @brief  构造函数，构造时枷锁
+     * @brief  Constructor, Constructive Flail
 	 *
      * @param lock 锁对象
+     * @param lock Lock Object
      */
 	TC_RW_WLockT(T& lock)
 		: _rwLock(lock),_acquired(false)
@@ -253,6 +286,7 @@ public:
 	}
     /**
 	 * @brief 析构时解锁
+     * @brief Unlock at destruct time
      */
 	~TC_RW_WLockT()
 	{
@@ -264,10 +298,12 @@ public:
 private:
 	/**
 	 *锁对象
+     *Lock Object
 	 */
 	const T& _rwLock;
     /**
      * 是否已经上锁
+     * Is it locked or not
      */
     mutable bool _acquired;
 
