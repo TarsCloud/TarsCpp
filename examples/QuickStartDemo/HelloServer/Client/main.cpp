a21c4e51a9647f4cbb33b78ca6bf65a4b516197c
ruanshudong <ruanshudong@qq.com>
2020-11-09 10:36:43 +0800

    fix tc_sha, __LITTLE_ENDIAN bug in windows/mac

--- examples/QuickStartDemo/HelloServer/Client/main.cpp
+++ examples/QuickStartDemo/HelloServer/Client/main.cpp
@@ -26,7 +26,7 @@ using namespace TestApp;
 Communicator* _comm;
 
 //static string helloObj = "TestApp.HelloServer.HelloObj@tcp -h 127.0.0.1 -p 8999";
-static string helloObj = "TestApp.HelloServer.HelloObj@tcp -h 127.0.0.1 -p 8199:tcp -h 127.0.0.1 -p 8299 -t 10000";
+static string helloObj = "TestApp.HelloServer.HelloObj@tcp -h 192.168.7.152 -p 8199:tcp -h 192.168.7.152 -p 8299 -t 10000";
 
 struct Param
 {
