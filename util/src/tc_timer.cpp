4d4a5e86166ee0bf5ec21ebd6116ee62319454d9
ruanshudong <ruanshudong@qq.com>
2021-07-27 10:04:05 +0800

    fix tc_timer memory leak and timer id duplicate bug

--- util/src/tc_timer.cpp
+++ util/src/tc_timer.cpp
@@ -21,37 +21,37 @@ namespace tars
 
 TC_Timer::~TC_Timer()
 {
-	stopTimer();
+    stopTimer();
 }
 
 void TC_Timer::startTimer(int numThread)
 {
-	if (numThread <= 0)
-	{
-		numThread = 1;
-	}
-
-	_terminate = false;
-
-	_tpool.init(numThread + 1);
-	_tpool.start();
-	_tpool.exec(std::bind(&TC_Timer::run, this));
+    if (numThread <= 0)
+    {
+        numThread = 1;
+    }
+    
+    _terminate = false;
+    
+    _tpool.init(numThread + 1);
+    _tpool.start();
+    _tpool.exec(std::bind(&TC_Timer::run, this));
 }
 
 void TC_Timer::stopTimer()
 {
-	if (_terminate)
+    if (_terminate)
     {
         return;
     }
-
-	{
-		std::unique_lock<std::mutex> lck(_mutex);
-		_terminate = true;
-		_cond.notify_all();
-	}
-
-	_tpool.stop();
+    
+    {
+        std::unique_lock<std::mutex> lck(_mutex);
+        _terminate = true;
+        _cond.notify_all();
+    }
+    
+    _tpool.stop();
 }
 
 bool TC_Timer::exist(int64_t uniqId,bool repeat )
@@ -67,123 +67,157 @@ bool TC_Timer::exist(int64_t uniqId,bool repeat )
     }
 }
 
-void TC_Timer::erase(int64_t uniqId)
+void TC_Timer::clear()
 {
 	std::lock_guard<std::mutex> lock(_mutex);
-	auto it = _mapEvent.find(uniqId);
-	if (it != _mapEvent.end()) 
-	{
-		auto itEvent = _mapTimer.find(it->second->_fireMillseconds);
-		if (itEvent != _mapTimer.end()) 
-		{
-			itEvent->second.erase(uniqId);
-			if (itEvent->second.empty()) 
-			{
-				_mapTimer.erase(itEvent);
-			}
-		}
-        it->second->_func = nullptr;
-		_mapEvent.erase(it);
-	}
+
+	_mapEvent.clear();
+	_mapTimer.clear();
+	_repeatIds.clear();
 }
 
-int64_t TC_Timer::post(const shared_ptr<TC_Timer::Func> &event, bool repeat)
+void TC_Timer::erase(int64_t uniqId)
 {
-	std::unique_lock<std::mutex> lock(_mutex);
+    std::lock_guard<std::mutex> lock(_mutex);
+    auto it = _mapEvent.find(uniqId);
+    
+    //LOG_CONSOLE_DEBUG << "before erase event!" << ",uniqId=" << uniqId << "|event size :" << _mapEvent.size() << "|timer size:" << _mapTimer.size() << endl;
+    if (it != _mapEvent.end())
+    {
+        auto itEvent = _mapTimer.find(it->second->_fireMillseconds);
+        if (itEvent != _mapTimer.end())
+        {
+            itEvent->second.erase(uniqId);
+            if (itEvent->second.empty())
+            {
+                _mapTimer.erase(itEvent);
+            }
+        }
+        it->second->_func = nullptr;
+        _mapEvent.erase(it);
+    }
+    _repeatIds.erase(uniqId);
+//    LOG_CONSOLE_DEBUG << "after erase event!" << ",uniqId=" << uniqId << "|event size :" << _mapEvent.size() << "|timer size:" << _mapTimer.size() << endl;
+}
 
-	int64_t uniqId = (int64_t) event.get();
+int64_t TC_Timer::post(const shared_ptr<TC_Timer::Func> &event ,bool repeat)
+{
+    std::unique_lock<std::mutex> lock(_mutex);
+    
+    int64_t uniqId = event->_uniqueId;
+    
     if (repeat)
     {
         _repeatIds.insert(uniqId);
     }
-
-	if (_mapEvent.find(uniqId) == _mapEvent.end()) 
-	{
-		_mapEvent[uniqId] = event;
-	}
-	_mapTimer[event->_fireMillseconds].insert(uniqId);
-	_cond.notify_one();
-	return uniqId;
+    
+    if (_mapEvent.find(uniqId) == _mapEvent.end())
+    {
+        _mapEvent[uniqId] = event;
+    }
+    _mapTimer[event->_fireMillseconds].insert(uniqId);
+//    LOG_CONSOLE_DEBUG << "post event! fire time:" << TC_Common::msToTimeString(event->_fireMillseconds ) << ",uniqId=" << uniqId << "|event size :"
+//                      << _mapEvent.size() << "|timer size:" << _mapTimer.size() <<",_func=" << &(event->_func)<< endl;
+    _cond.notify_one();
+    return uniqId;
 }
 
 void TC_Timer::fireEvent(const EVENT_SET &el)
 {
-	auto itList = el.begin();
-
-	while (itList != el.end()) 
-	{
-		shared_ptr<Func> func;
-
-		{
-			std::lock_guard<std::mutex> lock(_mutex);
-
-			auto it = _mapEvent.find(*itList);
-			if (it != _mapEvent.end()) 
-			{
-				func = it->second;
-				_mapEvent.erase(it);
-			}
-		}
-		if (func) 
-		{
-			//执行具体事件对象
-			_tpool.exec(func->_func);
-		}
-
-		++itList;
-	}
+    auto itList = el.begin();
+    
+    while (itList != el.end())
+    {
+        shared_ptr<Func> func;
+        
+        {
+            std::lock_guard<std::mutex> lock(_mutex);
+            
+            auto it = _mapEvent.find(*itList);
+            if (it != _mapEvent.end())
+            {
+                func = it->second;
+                _tmpEvent[it->first] = it->second;
+                _mapEvent.erase(it);
+            }
+        }
+        if (func)
+        {
+            //执行具体事件对象
+            _tpool.exec(func->_func);
+        }
+        
+        ++itList;
+    }
 }
 
 void TC_Timer::run()
 {
-	while (!_terminate) 
+    while (!_terminate)
     {
-		try 
+        try
         {
-			EVENT_SET el;
-
-			{
-				std::unique_lock<std::mutex> lock(_mutex);
-
-				auto it = _mapTimer.begin();
-
-				if (it != _mapTimer.end())
-                {
-					uint64_t ms = TC_TimeProvider::getInstance()->getNowMs();
-
-                    //cout << "now ms:" << TC_Common::tm2str(ms / 1000, "%Y-%m-%d %H:%M:%S") << "| it first:" << TC_Common::tm2str(it->first / 1000,  "%Y-%m-%d %H:%M:%S") << endl;
-					if (it->first <= ms) 
-                    {
-						//时间过了, 有事件需要触发了
-						el.swap(it->second);
-						_mapTimer.erase(it);
-					}
-					else 
-                    {
-						//时间还没到
-						ms = it->first - ms;
-						_cond.wait_for(lock, std::chrono::milliseconds(ms));
-					}
-				}
-				else 
-                {
-					//没有任何事件, 等待一下
-					_cond.wait_for(lock, std::chrono::milliseconds(1000));
-				}
-			}
-
-			if (!el.empty()) 
+            EVENT_SET el;
+            
             {
-				//触发相关所有事件
-				fireEvent(el);
-			}
+                std::unique_lock<std::mutex> lock(_mutex);
+                
+                auto it = _mapTimer.begin();
+                
+                if (it != _mapTimer.end())
+                {
+                    uint64_t ms = TC_TimeProvider::getInstance()->getNowMs();
+                    
+//                    LOG_CONSOLE_DEBUG << "now ms:" << TC_Common::msToTimeString(ms ) << "|timer:" << TC_Common::msToTimeString(it->first) << endl;
+                    if (it->first <= ms)
+                    {
+                        //时间过了, 有事件需要触发了
+                        el.swap(it->second);
+                        _mapTimer.erase(it);
+                    }
+                    else
+                    {
+                        //时间还没到
+                        ms = it->first - ms;
+                        _cond.wait_for(lock, std::chrono::milliseconds(ms));
+                    }
+                }
+                else
+                {
+                    //没有任何事件, 等待一下
+                    _cond.wait_for(lock, std::chrono::milliseconds(1000));
+                }
+            }
+            
+            if (!el.empty())
+            {
+                //触发相关所有事件
+                fireEvent(el);
+            }
+            
+        }
+        catch (exception &ex)
+        {
+            cerr << ex.what() << endl;
+        }
+    }
+}
 
-		}
-		catch (exception &ex) 
-		{
-			cerr << ex.what() << endl;
-		}
-	}
+uint32_t TC_Timer::genUniqueId()
+{
+    uint32_t i = ++_increaseId;
+    if (i == 0)
+    {
+        i = ++_increaseId;
+    }
+    
+    return i;
+}
+
+tuple<int64_t, int64_t, int64_t> TC_Timer::status()
+{
+    std::unique_lock<std::mutex> lock(_mutex);
+    return make_tuple(_tpool.getJobNum(), _mapEvent.size(), _repeatIds.size());
 }
 }
 
