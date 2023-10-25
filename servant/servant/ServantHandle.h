21b2932b733f7aff2088fcf686f23f2a72cb93df
ruanshudong <ruanshudong@qq.com>
2023-10-25 17:30:50 +0800

    fix servanthandle only keep one servant

--- servant/servant/ServantHandle.h
+++ servant/servant/ServantHandle.h
@@ -200,7 +200,8 @@ protected:
     /**
      * 处理对象
      */
-    unordered_map<string, ServantPtr> _servants;
+    ServantPtr _servant;
+//    unordered_map<string, ServantPtr> _servants;
 
 
 // #ifdef TARS_OPENTRACKING
