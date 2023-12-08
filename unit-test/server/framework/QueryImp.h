95987cfcaefdf929abc57e66ad02bf8fde680b2e
ruanshudong <ruanshudong@qq.com>
2023-12-08 21:53:41 +0800

    support master-slave mode in frameworkPro

--- unit-test/server/framework/QueryImp.h
+++ unit-test/server/framework/QueryImp.h
@@ -70,6 +70,30 @@ public:
      * 根据id获取对象同set endpoint列表
      */
     Int32 findObjectByIdInSameSet(const std::string & id,const std::string & setId,vector<tars::EndpointF> &activeEp,vector<tars::EndpointF> &inactiveEp, tars::CurrentPtr current);
+
+    /** 注册id变化的通知, 通知时会通知所有的变化内容(企业版功能)
+     *
+     * @param ids         对象名称
+     * @param name       当前模块名称
+     * @return:  0-成功  others-失败
+     */
+    Int32 registerChange(const vector<string> &ids, const string &name, CurrentPtr current) { return -1; };
+
+    /** 注册id变化的通知, 通知时后需要自己主动find(企业版功能)
+     *
+     * @param id         对象名称
+     * @param name       当前模块名称
+     * @return:  0-成功  others-失败
+     */
+    Int32 registerQuery(const string &id, const string &name, CurrentPtr current) { return -1; };
+
+    /**
+     * 获取锁, 实现业务服务一主多备的模式(企业版功能)
+     * @return 0: 获取锁成功； 1：获取锁失败； 2： 数据异常， -1：其他异常
+     */
+    Int32 getLocker(const tars::GetMasterSlaveLock &req, CurrentPtr current) { return -1;};
+
+
 private:
     /**
      * 打印按天日志
