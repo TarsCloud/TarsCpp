a82656406902f3459561d464b361882277be13dc
ruanshudong <ruanshudong@qq.com>
2026-07-24 09:17:20 +0800

    update tars stress server config & scripts

--- examples/StressDemo/TarsStressClient/teststress.sh
+++ examples/StressDemo/TarsStressClient/teststress.sh
@@ -1,17 +1,7 @@
 #!/usr/bin/env bash
 
-if [[ $# -lt 3 || $# -gt 4 ]]; then
-        echo "usage: $0 ProcessNum ThreadNum Size [sync|async]" >&2
-        exit 1
-fi
+#bin/TarsStressServer --config=../examples/StressDemo/TarsStressServer/config.conf 
+#bin/TarsStressClient 10 100000  "TestApp.StressServer.StressObj@tcp -h 127.0.0.1 -p 9300 -t 10000" 100 sync
+#bin/TarsStressClient 10 100000  "TestApp.StressServer.StressObj@tcp -h 127.0.0.1 -p 9300 -t 10000" 100 async
 
-mode="${4:-sync}"
-if [[ "$mode" != "sync" && "$mode" != "async" ]]; then
-        echo "CallMode must be sync or async" >&2
-        exit 1
-fi
 
-for((i=1; i<=$1; i++)); do {
-        ./TarsStressClient "$2" 1500000 Test.StressServer.StressObj "$3" "$mode"
-} & done
-wait
